69d460ff4e298f33ee456421b7f583a34fa389a4
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-07 19:55:42 +0300

    added creating html and modified assaignment 2

--- assaignment-2/app.js
+++ assaignment-2/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.use('/', (req, res, nex) => {
+app.get('/', (req, res, next) => {
 	res.send('<h1>Hello </h1>');
+	next();
+});
+
+app.get('/users', (req, res, next) => {
+	res.send('<h1>Users</h1>');
+});
+
+app.use((req, res, next) => {
+	res.status(404).send('<h1>Page Not Found!</h1>');
 });
 
 app.listen(3000, () => {
