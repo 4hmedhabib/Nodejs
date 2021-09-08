385add1c99b04ddd4179d3899a5584e6c89b40ae
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-08 11:38:52 +0300

    some files re updated

--- Express.js/MVC/00-starting-setup/models/products.js
+++ Express.js/MVC/00-starting-setup/models/products.js
@@ -1,19 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+const getProdcutsHelper = (cb) => {
+	const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+	fs.readFile(p, (err, fileContent) => {
+		if (err) {
+			cb([]);
+		} else {
+			cb(JSON.parse(fileContent));
+		}
+	});
+};
 module.exports = class Product {
 	constructor(t) {
 		this.title = t;
 	}
 
 	save() {
-		const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-		fs.readFile(p, (err, fileContent) => {
-			let products = [];
-			if (!err) {
-				products = JSON.parse(fileContent);
-			}
-			console.log(products);
+		getProdcutsHelper((products) => {
 			products.push(this);
 			fs.writeFile(p, JSON.stringify(products), (err) => {
 				console.log(err);
@@ -22,12 +28,6 @@ module.exports = class Product {
 	}
 
 	static fetchAll(cb) {
-		const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-		fs.readFile(p, (err, fileContent) => {
-			if (err) {
-				cb([]);
-			}
-			cb(JSON.parse(fileContent));
-		});
+		getProdcutsHelper(cb);
 	}
 };
