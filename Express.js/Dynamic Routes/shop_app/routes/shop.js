fc6a3ab86b3edbb5efa7d44318b8fa77a5eb9ae3
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-08 20:32:32 +0300

    added more files

--- Express.js/Dynamic Routes/shop_app/routes/shop.js
+++ Express.js/Dynamic Routes/shop_app/routes/shop.js
@@ -14,6 +14,10 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
