19291fae022816b418e94fd48e789d9d79f86304
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-08 20:28:57 +0300

    added more files

--- Express.js/Dynamic Routes/shop_app/controllers/shop.js
+++ Express.js/Dynamic Routes/shop_app/controllers/shop.js
@@ -1,47 +1,53 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-	Product.fetchAll((products) => {
-		res.render('shop/product-list', {
-			prods: products,
-			pageTitle: 'All Products',
-			path: '/products'
-		});
-	});
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
 };
 
 exports.getProduct = (req, res, next) => {
-	const productId = req.params.productId;
-	Product.findById(productId, (products) => {});
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
-	Product.fetchAll((products) => {
-		res.render('shop/index', {
-			prods: products,
-			pageTitle: 'Shop',
-			path: '/'
-		});
-	});
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
 };
 
 exports.getCart = (req, res, next) => {
-	res.render('shop/cart', {
-		path: '/cart',
-		pageTitle: 'Your Cart'
-	});
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
 };
 
 exports.getOrders = (req, res, next) => {
-	res.render('shop/orders', {
-		path: '/orders',
-		pageTitle: 'Your Orders'
-	});
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
 };
 
 exports.getCheckout = (req, res, next) => {
-	res.render('shop/checkout', {
-		path: '/checkout',
-		pageTitle: 'Checkout'
-	});
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
 };
