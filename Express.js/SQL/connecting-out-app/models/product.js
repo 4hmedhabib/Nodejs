0bdccdb38caae446c5ce2c118409c7a30796706d
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-09 11:54:03 +0300

    added sequelize

--- Express.js/SQL/connecting-out-app/models/product.js
+++ Express.js/SQL/connecting-out-app/models/product.js
@@ -1,30 +1,30 @@
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
-const Cart = require('./cart');
+const sequelize = require('../util/database');
 
-module.exports = class Product {
-	constructor(id, title, imageUrl, description, price) {
-		this.id = id;
-		this.title = title;
-		this.imageUrl = imageUrl;
-		this.description = description;
-		this.price = price;
+const Product = sequelize.define('product', {
+	id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		primaryKey: true,
+		allowNull: false
+	},
+	title: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	price: {
+		type: Sequelize.DOUBLE,
+		allowNull: false
+	},
+	imageUrl: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	description: {
+		type: Sequelize.STRING,
+		allowNull: false
 	}
+});
 
-	save() {
-		return db.execute('INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)', [
-			this.title,
-			this.price,
-			this.description,
-			this.imageUrl
-		]);
-	}
-
-	static deleteById(id) {}
-
-	static fetchAll() {
-		return db.execute('SELECT * FROM products');
-	}
-
-	static findById(id) {}
-};
+module.exports = Product;
--- Express.js/SQL/connecting-out-app/util/database.js
+++ Express.js/SQL/connecting-out-app/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-	host: 'localhost',
-	user: 'root',
-	database: 'node_complete',
-	password: ''
+const sequelize = new Sequelize('node_complete', 'root', '', {
+	dialect: 'mysql',
+	host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
