63065d2f8618314abba1c02a1cee17de282aeb1e
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-14 20:47:14 +0300

    added more files and updated

--- Express.js/SQL/Shop App/controllers/admin.js
+++ Express.js/SQL/Shop App/controllers/admin.js
@@ -13,7 +13,6 @@ exports.postAddProduct = (req, res, next) => {
 	const imageUrl = req.body.imageUrl;
 	const price = req.body.price;
 	const description = req.body.description;
-
 	req.user
 		.createProduct({
 			title: title,
@@ -22,6 +21,7 @@ exports.postAddProduct = (req, res, next) => {
 			description: description
 		})
 		.then((result) => {
+			// console.log(result);
 			console.log('Created Product');
 			res.redirect('/admin/products');
 		})
@@ -36,8 +36,11 @@ exports.getEditProduct = (req, res, next) => {
 		return res.redirect('/');
 	}
 	const prodId = req.params.productId;
-	Product.findByPk(prodId)
-		.then((product) => {
+	req.user
+		.getProducts({ where: { id: prodId } })
+		// Product.findByPk(prodId)
+		.then((products) => {
+			const product = products[0];
 			if (!product) {
 				return res.redirect('/');
 			}
@@ -59,21 +62,22 @@ exports.postEditProduct = (req, res, next) => {
 	const updatedDesc = req.body.description;
 	Product.findByPk(prodId)
 		.then((product) => {
-			(product.title = updatedTitle),
-				(product.price = updatedPrice),
-				(product.imageUrl = updatedImageUrl),
-				(product.description = updatedDesc);
-			product.save();
+			product.title = updatedTitle;
+			product.price = updatedPrice;
+			product.description = updatedDesc;
+			product.imageUrl = updatedImageUrl;
+			return product.save();
 		})
 		.then((result) => {
-			console.log('Updated The Product');
+			console.log('UPDATED PRODUCT!');
 			res.redirect('/admin/products');
 		})
 		.catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-	Product.findAll()
+	req.user
+		.getProducts()
 		.then((products) => {
 			res.render('admin/products', {
 				prods: products,
@@ -91,7 +95,7 @@ exports.postDeleteProduct = (req, res, next) => {
 			return product.destroy();
 		})
 		.then((result) => {
-			console.log('Deleted The Product');
+			console.log('DESTROYED PRODUCT');
 			res.redirect('/admin/products');
 		})
 		.catch((err) => console.log(err));
--- Express.js/SQL/Shop App/models/user.js
+++ Express.js/SQL/Shop App/models/user.js
@@ -3,20 +3,14 @@ const Sequelize = require('sequelize');
 const sequelize = require('../util/database');
 
 const User = sequelize.define('user', {
-	id: {
-		type: Sequelize.INTEGER,
-		autoIncrement: true,
-		primaryKey: true,
-		allowNull: false
-	},
-	name: {
-		type: Sequelize.STRING,
-		allowNull: false
-	},
-	email: {
-		type: Sequelize.STRING,
-		allowNull: false
-	}
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: Sequelize.STRING,
+  email: Sequelize.STRING
 });
 
 module.exports = User;
