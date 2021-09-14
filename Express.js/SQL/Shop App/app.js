63277e8b51dd6d62aabf69947d600f4db59f7b9f
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-14 20:08:08 +0300

    added more files

--- Express.js/SQL/Shop App/app.js
+++ Express.js/SQL/Shop App/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
@@ -20,20 +22,42 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+	User.findByPk(1)
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// Product.belongsTo(User);
+Product.belongsTo(User);
 User.hasMany(Product);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-	.sync({ force: true })
+	// .sync({ force: true })
+	.sync()
 	.then((result) => {
-		app.listen(3001, () => {
-			console.log('SERVER RUNNING PORT: 3001');
-		});
+		User.findByPk(1)
+			.then((user) => {
+				if (!user) {
+					return User.create({ name: 'Axmedd', email: 'test@gmail.com' });
+				}
+				return user;
+			})
+			.then((user) => {
+				console.log(user);
+				app.listen(3001, () => {
+					console.log('SERVER RUNNING PORT: 3001');
+				});
+			});
 	})
 	.catch((err) => {
 		console.log(err);
--- Express.js/SQL/Shop App/controllers/admin.js
+++ Express.js/SQL/Shop App/controllers/admin.js
@@ -13,12 +13,14 @@ exports.postAddProduct = (req, res, next) => {
 	const imageUrl = req.body.imageUrl;
 	const price = req.body.price;
 	const description = req.body.description;
-	Product.create({
-		title: title,
-		price: price,
-		imageUrl: imageUrl,
-		description: description
-	})
+
+	req.user
+		.createProduct({
+			title: title,
+			price: price,
+			imageUrl: imageUrl,
+			description: description
+		})
 		.then((result) => {
 			console.log('Created Product');
 			res.redirect('/admin/products');
