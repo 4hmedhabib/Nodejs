674eee6762ff185291fbdf492faf599c8602d43a
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-16 14:08:30 +0300

    added and updated more files

--- Express.js/Authentication/app.js
+++ Express.js/Authentication/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -16,6 +18,7 @@ const store = new MongoDBStore({
 	uri: MONGODB_URI,
 	collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -34,6 +37,8 @@ app.use(
 		store: store
 	})
 );
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
 	if (!req.session.user) {
@@ -47,6 +52,12 @@ app.use((req, res, next) => {
 		.catch((err) => console.log(err));
 });
 
+app.use((req, res, next) => {
+	res.locals.isAuthenticated = req.session.isLoggedIn;
+	res.locals.csrfToken = req.csrfToken();
+	next();
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
@@ -56,18 +67,6 @@ app.use(errorController.get404);
 mongoose
 	.connect(MONGODB_URI)
 	.then((result) => {
-		User.findOne().then((user) => {
-			if (!user) {
-				const user = new User({
-					name: 'Max',
-					email: 'max@test.com',
-					cart: {
-						items: []
-					}
-				});
-				user.save();
-			}
-		});
 		app.listen(3000);
 	})
 	.catch((err) => {
